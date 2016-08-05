dc75382a80b95dd8548556afd02c5c71a000859d
ranyehushua <ranyehushua@gmail.com>
2016-08-04 18:33:42 -0600

    Added OC4D Logo to Wiki

--- wiki/conf/LocalSettings.php
+++ wiki/conf/LocalSettings.php
@@ -45,7 +45,7 @@ $wgStylePath = "$wgScriptPath/skins";
 
 ## The relative URL path to the logo.  Make sure you change this from the default,
 ## or else you'll overwrite your logo when you upgrade!
-$wgLogo = "$wgScriptPath/resources/assets/wiki.png";
+$wgLogo = "$wgScriptPath/resources/assets/oc4d/OC4D_Logo.png";
 
 ## UPO means: this is also a user preference option
 
--- wiki/docker-mediawiki/docker-entrypoint.sh
+++ wiki/docker-mediawiki/docker-entrypoint.sh
@@ -5,31 +5,36 @@ set -e
 : ${MEDIAWIKI_SITE_NAME:=MediaWiki}
 
 if ! [ -e index.php -a -e includes/DefaultSettings.php ]; then
-	echo >&2 "MediaWiki not found in $(pwd) - copying now..."
+    echo >&2 "MediaWiki not found in $(pwd) - copying now..."
 
-	if [ "$(ls -A)" ]; then
-		echo >&2 "WARNING: $(pwd) is not empty - press Ctrl+C now if this is an error!"
-		( set -x; ls -A; sleep 10 )
-	fi
-	tar cf - --one-file-system -C /usr/src/mediawiki . | tar xf -
-	echo >&2 "Complete! MediaWiki has been successfully copied to $(pwd)"
+    if [ "$(ls -A)" ]; then
+        echo >&2 "WARNING: $(pwd) is not empty - press Ctrl+C now if this is an error!"
+        ( set -x; ls -A; sleep 10 )
+    fi
+    tar cf - --one-file-system -C /usr/src/mediawiki . | tar xf -
+    echo >&2 "Complete! MediaWiki has been successfully copied to $(pwd)"
 fi
 
 : ${MEDIAWIKI_SHARED:=/var/www-shared/html}
 if [ -d "$MEDIAWIKI_SHARED" ]; then
-	# If there is no LocalSettings.php but we have one under the shared
-	# directory, symlink it
-	if [ -e "$MEDIAWIKI_SHARED/LocalSettings.php" -a ! -e LocalSettings.php ]; then
-		ln -s "$MEDIAWIKI_SHARED/LocalSettings.php" LocalSettings.php
-	fi
+    # If there is no LocalSettings.php but we have one under the shared
+    # directory, symlink it
+    if [ -e "$MEDIAWIKI_SHARED/LocalSettings.php" -a ! -e LocalSettings.php ]; then
+        ln -s "$MEDIAWIKI_SHARED/LocalSettings.php" LocalSettings.php
+    fi
 
-	# If the images directory only contains a README, then link it to
-	# $MEDIAWIKI_SHARED/images, creating the shared directory if necessary
-	if [ "$(ls images)" = "README" -a ! -L images ]; then
-		rm -fr images
-		mkdir -p "$MEDIAWIKI_SHARED/images"
-		ln -s "$MEDIAWIKI_SHARED/images" images
-	fi
+    # If the images directory only contains a README, then link it to
+    # $MEDIAWIKI_SHARED/images, creating the shared directory if necessary
+    if [ "$(ls images)" = "README" -a ! -L images ]; then
+        rm -fr images
+        mkdir -p "$MEDIAWIKI_SHARED/images"
+        ln -s "$MEDIAWIKI_SHARED/images" images
+    fi
+fi
+
+# If the assets/oc4d directory does not already exist
+if [ -d "$MEDIAWIKI_SHARED/assets/oc4d" ]; then
+    ln -s "$MEDIAWIKI_SHARED/assets/oc4d" resources/assets/oc4d
 fi
 
 
