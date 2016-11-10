89c5a315540b792f49bff5b5d8e5a0004f2644ea
ranyehushua <ranyehushua@gmail.com>
2016-11-10 12:49:42 -0700

    added logic to set env variable on stop and build scripts

--- stop.sh
+++ stop.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-WEB_PORT=80 docker-compose stop
+# Default to development environment
+: ${ENVIRONMENT:=DEV}
+
+COMPOSE_PARAMS=''
+
+case $ENVIRONMENT in
+    DEV)
+        VAR_FILE='dev.sh'
+        ;;
+    PRODUCTION)
+        VAR_FILE='production.sh'
+        COMPOSE_PARAMS='-d'
+        ;;
+    *)
+        >&2 echo "Unknown environment: $ENVIRONMENT"
+        exit 127
+        ;;
+esac
+
+echo "Running environment: $ENVIRONMENT"
+
+source "env/$VAR_FILE"
+
+docker-compose stop
